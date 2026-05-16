ef4211e067a4eea6c6141c9276fc71a28a306f98
shorouk <shoroukezz331@gmail.com>
2026-05-16 20:16:47 +0300

    Create and integrate MyNewComponent

---
+++ my-new-project/src/MyNewComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyNewComponent = () => {
+  return (
+    <div className="p-4 bg-gray-100 rounded-lg">
+      <h2 className="text-xl font-bold">هذا هو المكون الجديد!</h2>
+      <p>تقدري تضيفي هنا أي كود HTML أو منطق برمجي.</p>
+    </div>
+  );
+};
+
+export default MyNewComponent;
